8899114884d18907f7239fb76deb6a36c5ffcc27
lcsrbr <93lucasribeiro@gmail.com>
2022-04-06 15:04:45 -0300

    tipos primitivos

---
+++ 01Fundamentos/Bloco04JavaScript/Scripts.js
@@ -0,0 +1,51 @@
+/*
+let age = 20;
+age = 30;
+
+let name = "Hulk";
+
+let CamelCase = "Corcova de camelo";
+
+const number = 50;
+
+console.log(age);
+console.log(age, name, CamelCase);
+console.log (number);
+*/
+
+
+
+/* VARIÁVEIS */
+
+const myName = "Lucas";
+const birthCity = "Belo Horizonte";
+var birthYear = 1993;
+
+console.log("Meu nome é " + myName + ", Nasci no ano de " + birthYear + ", Na cidade de " + birthCity+ ".")
+
+birthYear = 2030;
+
+console.log (birthYear);
+
+
+let movie = "Avengers"; //string literal: dentro de aspas, sequencia de caracteres
+
+let score = 10; //number literal: declaração de numero
+
+let isValid = true; //valor booleano (true false) 
+
+let name; //undefined: declaração indefinida
+
+let color = null; //nulo
+
+/* TIPOS PRIMITIVOS */
+
+const base = 5;
+var height = 8;
+
+const area = base * height;
+
+const perimeter = 2*(base+height);
+
+console.log("área = " + area)
+console.log("perímetro = " + perimeter)
