49eb6b1e9b1d32acd217e4090032bbc766bf6d66
lcsrbr <93lucasribeiro@gmail.com>
2022-04-06 14:48:12 -0300

    iniciando conteúdo JavaScript

---
+++ 01Fundamentos/Bloco04JavaScript/Scritps.js
@@ -0,0 +1,25 @@
+/*
+let age = 20;
+age = 30;
+
+let name = "Hulk";
+
+let CamelCase = "Corcova de camelo";
+
+const number = 50;
+
+console.log(age);
+console.log(age, name, CamelCase);
+console.log (number);
+*/
+
+const myName = "Lucas";
+const birthCity = "Belo Horizonte";
+var birthYear = 1993;
+
+console.log("Meu nome é " + myName + ", Nasci no ano de " + birthYear + ", Na cidade de " + birthCity+ ".")
+
+birthYear = 2030;
+
+console.log (birthYear);
+
