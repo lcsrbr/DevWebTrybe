c20cc5ccf8a79d18b4db36396e4da2ebbad65aea
lcsrbr <93lucasribeiro@gmail.com>
2022-04-06 15:35:45 -0300

    operadores lógicos &&

--- 01Fundamentos/Bloco04JavaScript/Scripts.js
+++ 01Fundamentos/Bloco04JavaScript/Scripts.js
@@ -21,11 +21,11 @@ const myName = "Lucas";
 const birthCity = "Belo Horizonte";
 var birthYear = 1993;
 
-console.log("Meu nome é " + myName + ", Nasci no ano de " + birthYear + ", Na cidade de " + birthCity+ ".")
+console.log("Meu nome é " + myName + ", Nasci no ano de " + birthYear + ", Na cidade de " + birthCity + ".")
 
 birthYear = 2030;
 
-console.log (birthYear);
+console.log(birthYear);
 
 
 let movie = "Avengers"; //string literal: dentro de aspas, sequencia de caracteres
@@ -45,7 +45,7 @@ var height = 8;
 
 const area = base * height;
 
-const perimeter = 2*(base+height);
+const perimeter = 2 * (base + height);
 
 console.log("área = " + area)
 console.log("perímetro = " + perimeter)
@@ -86,11 +86,34 @@ if (trybe >= 14 && trybe < 14.40) {
 const nota = 100;
 
 if (nota >= 80 && nota <= 100) {
-    console.log ("Parabéns! Você foi aprovado(a)!");
+    console.log("Parabéns! Você foi aprovado(a)!");
 } else if (nota >= 60 && nota < 80) {
-    console.log ("Você está na lista de espera");
+    console.log("Você está na lista de espera");
 } else if (nota < 60) {
-    console.log ("Você foi reprovado");
+    console.log("Você foi reprovado");
 } else {
     false
 }
+
+/* OPERADORES LÓGICOS */
+
+/* && */
+
+const currentHour = 10;
+var message = "";
+
+if (currentHour >= 22) {
+message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar ;D";
+} else if (currentHour >=14 && currentHour < 18) {
+    message= "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+} else if (currentHour>= 4 && currentHour < 11) {
+    message = "Hmm, cheiro de café recém passado!"
+} else {
+    false
+}
+
+console.log("São " + currentHour + " horas. " + message)
