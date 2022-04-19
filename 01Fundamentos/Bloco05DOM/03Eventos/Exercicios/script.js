a32eb6d39ba247e4579d785dd94e6beffc79d2e8
lcsrbr <93lucasribeiro@gmail.com>
2022-04-19 10:55:16 -0300

    ex4

--- 01Fundamentos/Bloco05DOM/03Eventos/Exercicios/script.js
+++ 01Fundamentos/Bloco05DOM/03Eventos/Exercicios/script.js
@@ -16,9 +16,9 @@ createDaysOfTheWeek();
 // Escreva seu código abaixo.
 
 //Ex1
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function getDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   for (let i = 0; i < dezDaysList.length; i += 1) {
     const ulDays = document.getElementById("days");
@@ -76,3 +76,11 @@ function botaoCorFeriados() {
 botaoCorFeriados ()
 
 //Ex4
+function buttonSextaFeira () {
+let buttonContainer = document.querySelector(".buttons-container");
+let createButtonFriday = document.createElement("button");
+buttonContainer.appendChild(createButtonFriday);
+createButtonFriday.innerText = "Sexta-feira"
+createButtonFriday.id = "btn-friday";
+}
+buttonSextaFeira ()
