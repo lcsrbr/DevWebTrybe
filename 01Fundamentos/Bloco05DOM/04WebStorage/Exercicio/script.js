6bc8642907e408052bd7d4df4c25b9e9a5bc81a2
lcsrbr <93lucasribeiro@gmail.com>
2022-05-03 12:35:16 -0300

    ex 5.4

--- 01Fundamentos/Bloco05DOM/04WebStorage/Exercicio/script.js
+++ 01Fundamentos/Bloco05DOM/04WebStorage/Exercicio/script.js
@@ -1,68 +1,109 @@
-let fundoBranco = document.getElementById("fundoBranco")
-let fundoPreto = document.getElementById("fundoPreto")
-let fundoAzul = document.getElementById("fundoAzul")
 
-let fundo;
-localStorage.setItem('background', fundo)
-localStorage.setItem('font', 'teste')
+///// cor do fundo
+const fundoBranco = document.getElementById("fundoBranco")
+const fundoPreto = document.getElementById("fundoPreto")
+const fundoAzul = document.getElementById("fundoAzul")
 
-
-fundoBranco.addEventListener('click', function(){
-    fundo = "white"
-    localStorage.background = fundo
-   document.body.style.backgroundColor = fundo
-   document.body.style.color = "black"
-
+fundoBranco.addEventListener('click', () => {
+    document.body.style.backgroundColor = "whitesmoke"
+    localStorage.setItem('fundo', "whitesmoke")
 })
 
-fundoPreto.addEventListener('click', function(){
-    fundo = "black"
-    localStorage.background = fundo
-
-   document.body.style.backgroundColor = fundo
-    document.body.style.color = "white"
-
-})
-
-fundoAzul.addEventListener('click', function(){
-    fundo = "blue"
-    localStorage.background = fundo
-
-    document.body.style.backgroundColor = fundo
-    document.body.style.color = "white"
-
-   // document.body.style.color = "lightgrey"
-})
-
-localStorage.getItem('background')
-let cor = document.body.style.color
-
-
-
-/* 
-let fundoBranco = document.getElementById("fundoBranco")
-let fundoPreto = document.getElementById("fundoPreto")
-let fundoAzul = document.getElementById("fundoAzul")
-
-fundoBranco.addEventListener('click', function(){
+fundoPreto.addEventListener('click', () => {
     document.body.style.backgroundColor = "black"
-    document.body.style.color = "white"
+    localStorage.setItem('fundo', "black")
 })
 
-fundoPreto.addEventListener('click', function(){
-    document.body.style.backgroundColor = "white"
-    document.body.style.color = "black"
+fundoAzul.addEventListener('click', () => {
+    document.body.style.backgroundColor = "lightblue"
+    localStorage.setItem('fundo', "lightblue")
 })
 
-fundoAzul.addEventListener('click', function(){
-    document.body.style.backgroundColor = "darkblue"
-    document.body.style.color = "lightgrey"
+
+////cor do texto
+const textoMarrom = document.getElementById("textoMarrom")
+const textoVermelho = document.getElementById("textoVermelho")
+const textoVerde = document.getElementById("textoVerde")
+
+textoMarrom.addEventListener('click', () => {
+    document.body.style.color = "brown"
+    localStorage.setItem('cor', "brown")
 })
 
-let cor = document.body.style.color
+textoVermelho.addEventListener('click', () => {
+    document.body.style.color = "red"
+    localStorage.setItem('cor', "red")
+})
 
-localStorage.setItem('background', document.body.style.backgroundColor = "black")
-localStorage.setItem('font', cor)
+textoVerde.addEventListener('click', () => {
+    document.body.style.color = "green"
+    localStorage.setItem('cor', "green")
+})
 
-let fundo = localStorage.background
- */
+////tamanho da fonte
+const fontePequena = document.getElementById("fontePequena")
+const fonteMedia = document.getElementById("fonteMedia")
+const fonteGrande = document.getElementById("fonteGrande")
+
+fontePequena.addEventListener('click', () => {
+    document.body.style.fontSize = "10px"
+    localStorage.setItem('size', "10px")
+})
+
+fonteMedia.addEventListener('click', () => {
+    document.body.style.fontSize = "20px"
+    localStorage.setItem('size', "20px")
+})
+fonteGrande.addEventListener('click', () => {
+    document.body.style.fontSize = "30px"
+    localStorage.setItem('size', "30px")
+})
+
+
+///espaçamento
+const espPequeno = document.getElementById("espPequeno")
+const espMedio = document.getElementById("espMedio")
+const espGrande = document.getElementById("espGrande")
+
+espPequeno.addEventListener('click', () => {
+    document.body.style.lineHeight = "15px"
+    localStorage.setItem('espac', "15px")
+})
+
+espMedio.addEventListener('click', () => {
+    document.body.style.lineHeight = "20px"
+    localStorage.setItem('espac', "20px")
+})
+espGrande.addEventListener('click', () => {
+    document.body.style.lineHeight = "30px"
+    localStorage.setItem('espac', "30px")
+})
+
+///font family
+const fonteVerdana = document.getElementById("fonteVerdana")
+const fonteCourier = document.getElementById("fonteCourier")
+const fonteHelvetica = document.getElementById("fonteHelvetica")
+
+fonteVerdana.addEventListener('click', () => {
+    document.body.style.fontFamily = "Verdana"
+    localStorage.setItem('font', "Verdana")
+})
+
+fonteCourier.addEventListener('click', () => {
+    document.body.style.fontFamily = "Courier"
+    localStorage.setItem('font', "Courier")
+
+})
+
+fonteHelvetica.addEventListener('click', () => {
+    document.body.style.fontFamily = "Helvetica"
+    localStorage.setItem('font', "Helvetica")
+
+})
+
+document.body.style.backgroundColor = localStorage.fundo
+document.body.style.color = localStorage.cor
+document.body.style.fontSize = localStorage.size
+document.body.style.lineHeight = localStorage.espac
+document.body.style.fontFamily = localStorage.font
+
