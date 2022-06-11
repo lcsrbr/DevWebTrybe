a2c1f5977425848e22c79551fc85cabf2df5fbe1
lucas moura <93lucasribeiro@gmail.com>
2022-06-11 14:19:08 -0300

    melhorando organização

--- Módulo 01 - Fundamentos/Bloco 02 - Git, Github/01OQueEPraQueServe/trybeTesteGitHub/README.md
+++
@@ -1,3 +0,0 @@
-Repositório de exercícios do curso de Desenvolvimento Web da Trybe.
-
-Ainda ententendo como funciona isso aqui! ˆˆ
--- Módulo 02 - FrontEnd/Bloco 10 - Introdução ao React/01 - Hello World/testes react na mão/react.js
+++
@@ -1,7 +0,0 @@
-const div = document.getElementById('teste')
-
-
-const hello = (React.createElement('p',null,'hello world'))
-
-
-ReactDOM.render(hello,div)
