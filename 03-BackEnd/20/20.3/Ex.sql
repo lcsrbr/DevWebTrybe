da2a1ce042585ceda932a229d96608ba567000c9
Lucas Moura <93lucasribeiro@gmail.com>
2022-09-16 12:24:22 -0300

    ex 20.3

---
+++ 03-BackEnd/20/20.3/Ex.sql
@@ -0,0 +1,9 @@
+USE PecasFornecedores;
+-- SELECT * FROM PecasFornecedores.Pecas WHERE name LIKE 'GR%'-- 
+-- SELECT * FROM Fornecimentos WHERE peca = 2 ORDER BY fornecedor ASC
+-- SELECT peca, Preco, fornecedor FROM Fornecimentos WHERE fornecedor LIKE '%N%'
+-- SELECT * FROM Fornecedores WHERE name LIKE '%LTDA%' ORDER BY name DESC
+-- SELECT * FROM Fornecedores WHERE code LIKE '%F%'
+-- SELECT * FROM Fornecimentos WHERE Preco BETWEEN 15 AND 40 ORDER BY Preco ASC
+-- COUNT(*) FROM Vendas WHERE DATE(order_date) BETWEEN '2018-04-15' AND '2019-07-30'
+ # 15/04/2018 e o dia 30/07/2019.
