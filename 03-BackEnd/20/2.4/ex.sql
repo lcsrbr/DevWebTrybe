4673b2aa895244808a9adb99fdcc24bc86aa3e54
Lucas Moura <93lucasribeiro@gmail.com>
2022-09-16 15:20:19 -0300

    ex bonus

--- 03-BackEnd/20/2.4/ex.sql
+++ 03-BackEnd/20/2.4/ex.sql
@@ -69,3 +69,20 @@ WHERE movie_id IN ('2','9');
 DELETE FROM Movies
 WHERE director = 'Andrew Staton';
 
+-- Exercício 8: Altere a classificação da tabela BoxOffice para 9.0 de todos os filmes que lucraram mais de 400 milhões no mercado interno.
+USE Pixar;
+UPDATE BoxOffice
+SET rating = '9.0' WHERE domestic_sales > '400000000'; 
+
+-- Exercício 9: Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.
+USE Pixar;
+UPDATE BoxOffice
+SET rating = '6.0' WHERE domestic_sales > '200000000' AND international_sales < '300000000'; 
+-- Exercício 10: Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
+SET SQL_SAFE_UPDATES = 0;
+
+USE Pixar;
+DELETE FROM BoxOffice
+WHERE movie_id IN (1,6,7,8);
+DELETE FROM Movies
+WHERE length_minutes < '100';
