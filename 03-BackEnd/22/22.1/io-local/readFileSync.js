f54067e7a6bc9d8b35dde62e5099e041089909bf
Lucas Moura <93lucasribeiro@gmail.com>
2022-09-27 16:27:44 -0300

    22

---
+++ 03-BackEnd/22/22.1/io-local/readFileSync.js
@@ -0,0 +1,24 @@
+const fs = require('fs').promises;
+
+async function main() {
+  try {
+    const data = await fs.readFile('./meu-arquivo.txt', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
+}
+
+main()
+
+
+async function main2() {
+  try {
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textãosasso');
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+}
+
+main2()
