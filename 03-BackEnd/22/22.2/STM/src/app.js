0b69d44fdc0df4af54b62b85eae82b4fdc72780d
Lucas Moura <93lucasribeiro@gmail.com>
2022-09-28 15:21:34 -0300

    stm

---
+++ 03-BackEnd/22/22.2/STM/src/app.js
@@ -0,0 +1,30 @@
+// src/app.js
+const express = require('express');
+
+const teams = [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPF',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+  ];
+
+const app = express();
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+    const newTeam = { ...req.body };
+    teams.push(newTeam);
+  
+    res.status(201).json({ team: newTeam });
+  });
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+module.exports = app;
